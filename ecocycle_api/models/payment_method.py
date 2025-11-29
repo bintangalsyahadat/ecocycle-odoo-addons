2d56907d5fc7999f638d18ee0ce648bd7bc8779b
BintangAl <bintangalsyahadat1@gmail.com>
2025-11-29 15:32:21 +0700

    [FIX] ecocycle_api: fix access payment transaction

--- ecocycle_api/models/payment_method.py
+++ ecocycle_api/models/payment_method.py
@@ -3,3 +3,8 @@ from odoo import api, fields, models
 class PaymentMethod(models.Model):
     _name = 'ecocycle.payment.method'
     _inherit = ['ecocycle.payment.method', 'api.resource']
+    
+    
+class PaymentTransaction(models.Model):
+    _name = 'payment.transaction'
+    _inherit = ['payment.transaction', 'api.resource']
