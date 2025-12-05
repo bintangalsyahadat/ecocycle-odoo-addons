d9eff6c5d4bddf38dcee881e17f6911c5dd24b65
BintangAl <bintangalsyahadat1@gmail.com>
2025-12-06 02:11:36 +0700

    [FIX] ecocycle_transaction: fix undefined field

--- ecocycle_transaction/models/purchase_transaction.py
+++ ecocycle_transaction/models/purchase_transaction.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models, _
+from math import floor
 
 
 class PurchaseTransaction(models.Model):
@@ -80,8 +81,8 @@ class PurchaseTransaction(models.Model):
     def update_partner_reward(self):
         for rec in self:
             if rec.state == 'purchased':
-                coin = total_amount
-                point = floor(total_amount / 1000)
+                coin = rec.total_amount
+                point = floor(rec.total_amount / 1000)
                 
                 if rec.planner_id:
                     rec.planner_id.point_rewarded = point
